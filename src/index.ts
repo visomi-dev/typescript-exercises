0bb7937242bbe914a133d2f2074b22b3e9fc5e02
Michael Villalba Sotelo <visomi.me+dev@gmail.com>
2021-07-27 18:28:41 -0500

    feat exercises

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// Algunas palabras reservadas:
+// number - tipo número
+// const - declarar constante (no cambia)
+// let - declarar variable
+// for - ciclos
+
+// declaramos y asignando el temporizador
+let timer: number = 0;
+
+// esperamos hasta el 60
+for (let index = 0; index < 60; index++) {
+    timer++;
+}
+
+// si ya pasaron 60 minutos
+if (timer == 60) {
+    console.log('Ya está cocinado')
+}
+
+// Imprimir mensaje "Hola mundo"
+// Guardar tu nombre una constante e imprimirla
+// Sumar 2 variables y mostrarlas a consola
